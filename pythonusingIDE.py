ea969970ead17eee656fa32d7094725276d60f4d
sayligadgil <sayligadgil06@gmail.com>
2026-06-13 23:02:26 +0530

    2. program to execute python code using IDE (VS Code / PyCharm / Jupyter)

---
+++ pythonusingIDE.py
@@ -0,0 +1,16 @@
+# Program to calculate the area and perimeter of a rectangle
+print("--- Rectangle Calculator ---")
+
+# Taking user inputs
+length = 5.0
+width = 3.0
+
+# Performing operations
+area = length * width
+perimeter = 2 * (length + width)
+
+# Displaying outputs using standard print formatting
+print("Length of Rectangle:", length)
+print("Width of Rectangle:", width)
+print("Calculated Area:", area)
+print("Calculated Perimeter:", perimeter)
